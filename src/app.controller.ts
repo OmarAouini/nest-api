20084e45f05e3277fa85d454ca3972d92524c6a1
OmarAouini <aouini.omar93@gmai.com>
2021-09-16 23:09:59 +0200

    init commit

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,13 +1,12 @@
-import { Controller, Get, HttpCode } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get('health')
-  @HttpCode(200)
-  health(): string {
-    return 'OK';
+  @Get()
+  getHello(): string {
+    return this.appService.getHello();
   }
 }
